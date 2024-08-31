9b919bad637c316c8dab8ecbb61ba6ee41657de5
Priyanshu Singh <125067602+TheUnderdog553@users.noreply.github.com>
2024-08-31 12:43:41 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+//alert("hello") we are using nodejs not browser
+
+let name = "priyanshu"
+let age = 20
+let isLoggedIn = true
+let state = null
+//number
+//bigint
+//string ""
+//boolean true/false
+//null => standalone value
+//undefined => value not defined
+//symbol => unique
+//object 
+//null is object 
+console.log(null);
+console.log(undefined);
+
+
+console.log(typeof 34);
+
+
+
---
+++ basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 144552
+let accountEmail = "priyanshu@gmail.com"
+var accountPassword = "12345"
+accountCity = "Sultanpur"
+let accountState
+//accountId = 2
+console.log(accountId);
+/* prefer not to use var coz of block and functional scope */
+accountEmail = "phbs@gmail.com"
+accountPassword = "1234"
+accountCity = "Chennai"
+console.table([accountState, accountId, accountEmail,accountPassword,accountCity])
