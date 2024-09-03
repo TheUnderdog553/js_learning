37ba70089f518130a1742b349dc5c63b3fbb3f1a
Priyanshu Singh <125067602+TheUnderdog553@users.noreply.github.com>
2024-09-03 03:08:53 +0000

    Math_and_num

---
+++ basics/nums_and_maths.js
@@ -0,0 +1,35 @@
+const score = 36
+// console.log(score);
+
+// const balance = new Number(100.523)
+//  console.log(balance);
+//  console.log(balance.toString().length);
+//  console.log(balance.toFixed(2));
+//  console.log(balance.toPrecision(3));// 101
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//++++++++++++++++Maths++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));//4
+// console.log(Math.round(-4.6));//-5
+// console.log(Math.round(4.6));//5
+// console.log(Math.ceil(4.2));//5
+// console.log(Math.floor(4.2));//4
+// console.log(Math.sqrt(64)); //8
+// console.log(Math.min(3,4,5,64));
+// console.log(Math.max(6,4,9,7));
+
+console.log((Math.random()*10)+1);// math.random() always give value between 0 and 1
+console.log(Math.floor((Math.random()*10)+1));
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1))+ min);
+
+
+
+
+
+
---
+++ basics/strings.js
@@ -0,0 +1,45 @@
+const name = "Priyanshu"
+const repoCount = 50
+// console.log(name + repoCount + "Value");
+
+// console.log(`Hello my name is ${name} and i have a repoCount of ${repoCount} `);
+
+const gameName = new String(`Priyanshu Singh`)
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);  // initiates an object
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName);
+
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('y'));
+
+// const newString = gameName.substring(0,4) // this is a method .. if we give negative values like(-8,4)... then it drops negative values and starts with 0
+// console.log(newString);
+
+// const anotherString = gameName.slice(-14,5) // we can take negative values in this
+// console.log(anotherString);
+// const newStringOne = " Priyanshu Singh "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+// console.log(newStringOne.trimStart());
+// console.log(newStringOne.trimEnd());
+
+const url = "https://google.com/priyanshu%20singh"
+console.log(url);
+console.log(url.replace('%20', '-'));
+console.log(url.includes('Singh'));// checks if the particular string is their in real string
+
+
+
+
+
+
+
+
+
+
+
+
+
+
